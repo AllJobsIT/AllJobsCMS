ab452521fcd630f725480fbc2f3d097ad764affc
Oleg Sheinov <oleg20064@gmail.com>
2024-07-09 15:18:43 +0200

    Поправил отображение неопределенного типа отношений работника

--- core/choices/relationship_type.py
+++ core/choices/relationship_type.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class RelationshipTypeChoice(models.IntegerChoices):
-    UNSPECIFIED = -1, "--------------------------"
+    UNSPECIFIED = -1, "--"
     EMPLOYEE = 0, _("Наемный")
     STAFF = 1, _("Штатный")
     AFFILIATE = 2, _("От партнера")
