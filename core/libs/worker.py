e3262fa7e7bc06c47f9e240786f39a610518ebde
Oleg Sheinov <oleg20064@gmail.com>
2024-07-11 14:37:59 +0200

    Поправил определение копий работников

--- core/libs/worker.py
+++ core/libs/worker.py
@@ -3,4 +3,5 @@ from django.apps import apps
 
 def get_similar_workers(instance):
     Worker = apps.get_model("core", "Worker")
-    return Worker.objects.filter(name=instance.name, birthday=instance.birthday).exclude(pk=instance.pk)
+    return Worker.objects.filter(name=instance.name, last_name=instance.last_name, surname=instance.surname,
+                                 birthday=instance.birthday).exclude(pk=instance.pk)
