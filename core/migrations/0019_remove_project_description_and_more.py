66f465bb38b192eb684c63bc7fe455cf590a55b4
Oleg Sheinov <oleg20064@gmail.com>
2024-07-21 16:48:22 +0200

    Поправил миграцию

--- core/migrations/0019_remove_project_description_and_more.py
+++ core/migrations/0019_remove_project_description_and_more.py
@@ -6,9 +6,7 @@ import wagtail.fields
 import wagtail.snippets.blocks
 from django.db import migrations, models
 
-
 class Migration(migrations.Migration):
-
     dependencies = [
         ('core', '0018_remove_worker_sales_rate_project_sales_rate'),
     ]
@@ -33,16 +31,31 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='project',
             name='vacancy',
-            field=models.ForeignKey(default=None, null=True, on_delete=django.db.models.deletion.CASCADE, to='core.vacancy', verbose_name='Vacancy for project'),
+            field=models.ForeignKey(default=None, null=True, on_delete=django.db.models.deletion.CASCADE,
+                                    to='core.vacancy', verbose_name='Vacancy for project'),
         ),
-        migrations.AlterField(
+        migrations.RemoveField(
             model_name='project',
             name='sales_rate',
-            field=wagtail.fields.StreamField([('cost_item', wagtail.blocks.StructBlock([('size', wagtail.blocks.IntegerBlock(label='Cost size')), ('currency', wagtail.snippets.blocks.SnippetChooserBlock('core.CurrencySnippet', label='Cost currency'))], label='Cost item'))], blank=True, null=True, verbose_name='Sales rate'),
+        ),
+        migrations.AddField(
+            model_name='project',
+            name='sales_rate',
+            field=wagtail.fields.StreamField([('cost_item', wagtail.blocks.StructBlock(
+                [('size', wagtail.blocks.IntegerBlock(label='Cost size')), ('currency',
+                                                                            wagtail.snippets.blocks.SnippetChooserBlock(
+                                                                                'core.CurrencySnippet',
+                                                                                label='Cost currency'))],
+                label='Cost item'))], blank=True, null=True, verbose_name='Sales rate'),
         ),
         migrations.AlterField(
             model_name='worker',
             name='salary',
-            field=wagtail.fields.StreamField([('cost_item', wagtail.blocks.StructBlock([('size', wagtail.blocks.IntegerBlock(label='Cost size')), ('currency', wagtail.snippets.blocks.SnippetChooserBlock('core.CurrencySnippet', label='Cost currency'))], label='Cost item'))], blank=True, null=True, verbose_name='Worker salary'),
+            field=wagtail.fields.StreamField([('cost_item', wagtail.blocks.StructBlock(
+                [('size', wagtail.blocks.IntegerBlock(label='Cost size')), ('currency',
+                                                                            wagtail.snippets.blocks.SnippetChooserBlock(
+                                                                                'core.CurrencySnippet',
+                                                                                label='Cost currency'))],
+                label='Cost item'))], blank=True, null=True, verbose_name='Worker salary'),
         ),
     ]
