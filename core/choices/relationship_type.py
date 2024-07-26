86287219bbdd1a8b5a2971f1de4e5baf102e3947
Oleg Sheinov <oleg20064@gmail.com>
2024-07-26 11:48:24 +0200

    https://notes.kostaff.ru/browse/INHOUSE-21
    Пофиксил название поля работодателя

--- core/choices/relationship_type.py
+++ core/choices/relationship_type.py
@@ -4,6 +4,6 @@ from django.utils.translation import gettext_lazy as _
 
 class RelationshipTypeChoice(models.IntegerChoices):
     UNSPECIFIED = -1, "--"
-    EMPLOYEE = 0, _("Employed")
+    EMPLOYEE = 0, _("Employee")
     STAFF = 1, _("Standard")
     AFFILIATE = 2, _("From Affiliate")
