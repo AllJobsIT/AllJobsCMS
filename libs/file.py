a0ad4b3be46ec251e4361e6e863f7029744ad360
Oleg Sheinov <oleg20064@gmail.com>
2025-02-24 13:03:19 +0100

    Фикс

--- libs/file.py
+++ libs/file.py
@@ -1,8 +1,6 @@
 
 
 def load_document(file):
-    import nltk
-    nltk.download("punkt")
     from langchain.document_loaders import UnstructuredFileLoader
     loader = UnstructuredFileLoader(file)
     data = loader.load()
