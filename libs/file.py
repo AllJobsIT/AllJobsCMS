9bba880c4fe955aa398fe9eb067eb68b90cccae4
Oleg Sheinov <oleg20064@gmail.com>
2025-02-24 12:39:34 +0100

    Добавил зависимости и скачивание пакета

--- libs/file.py
+++ libs/file.py
@@ -1,7 +1,8 @@
-import os
 
 
 def load_document(file):
+    import nltk
+    nltk.download("punkt")
     from langchain.document_loaders import UnstructuredFileLoader
     loader = UnstructuredFileLoader(file)
     data = loader.load()
